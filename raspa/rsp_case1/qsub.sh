b73b38f80a7ae04855799d0b8ecc32d2877e9d54
Richard Gowers <richardjgowers@gmail.com>
2016-07-20 12:23:52 +0100

    RSP: Updated qsub to work on eddie

--- raspa/rsp_case1/qsub.sh
+++ raspa/rsp_case1/qsub.sh
@@ -2,18 +2,22 @@
 
 #$ -N rsp_{pressure}
 #$ -cwd
-#$ -l h_rt=12:00:00
+#$ -l h_rt=36:00:00
 #$ -l h_vmem=3G
 
 . /etc/profile.d/modules.sh
 ulimit -s unlimited
 
+module load intel/2016
+
+export LD_LIBRARY_PATH=/exports/applications/apps/SL7/intel/parallel_studio_xe_2016/compilers_and_libraries/linux/mpi/intel64/bin:/exports/applications/apps/SL7/intel/parallel_studio_xe_2016/compilers_and_libraries/linux/mkl/lib/intel64:/exports/applications/apps/SL7/intel/parallel_studio_xe_2016/compilers_and_libraries/linux/../../inspector_xe/lib64:/exports/applications/apps/SL7/intel/parallel_studio_xe_2016/compilers_and_libraries/linux/ipp/lib/intel64:/exports/applications/apps/SL7/intel/parallel_studio_xe_2016/compilers_and_libraries/linux/lib/intel64:/usr/local/blcr/0.8.6_b4/lib64:/usr/local/blcr/0.8.6_b4/lib:/exports/applications//gridengine/2011.11p1_155/lib/linux-x64
+
 cat /proc/cpuinfo > cpuinfo.{pressure}
 
 # Ensure that RASPA is linked in parent directory!
-export RASPA_DIR=../RASPA/
+export RASPA_DIR=/home/rgowers/RASPA/
 
-echo "Timing DLMonte case 1 at pressure {pressure}" > timing.out
+echo "Timing Raspa case 1 at pressure {pressure}" > timing.out
 date >> timing.out
 ./simulate simulation.input
 date >> timing.out
