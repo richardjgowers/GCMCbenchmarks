a497ed36d90bf694ac7823fdaa98ddd57d872db1
Richard Gowers <richardjgowers@gmail.com>
2016-07-27 16:12:43 +0100

    MUS: Updated making music simulations

--- make_music_sims.py
+++ make_music_sims.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python
+from __future__ import division
 
-def make_sims(pressure_values, suffix, destination):
-    pass
+import os
+import sys
+import shutil
+
+# Pressures to run in kPa
+PRESSURES = [5, 10, 20, 30, 40, 50, 60, 70]
+
+
+def make_sims(pressure_values, case, destination):
+    sourcedir = 'music/mus_{}'.format(case)
+
+    os.mkdir(os.path.join(destination, 'mus_{}'.format(case)))
+    for f in ['atom_atom_all', 'fluid_properties.dat', 'fugacity.dat',
+              'gcmc.ctr', 'intra', 'mol_mol_all', 'post.ctr', 'pressure.dat',
+              'qsub.sh', 'setpath']:
+        shutil.copy(os.path.join(sourcedir, f),
+                    os.path.join(destination, 'mus_{}'.format(case), f))
+    for d in ['atoms', 'molecules', 'pmaps_krista']:
+        shutil.copytree(os.path.join(sourcedir, '../', d),
+                        os.path.join(destination, d))
+
+
+if __name__ == '__main__':
+    try:
+        prefix, destination = sys.argv[1], sys.argv[2]
+    except IndexError:
+        raise SystemExit("Usage: {} templatedir destination".format(sys.argv[0]))
+    else:
+        make_sims(PRESSURES, prefix, destination)
