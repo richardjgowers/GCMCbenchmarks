0fc6fe9ad73313ffbf71a92267c8962060f3822d
Richard Gowers <richardjgowers@gmail.com>
2016-08-03 14:59:26 +0100

    CAS: Updated run times for cases 2&3

--- cassandra/cas_case2/qsub.sh
+++ cassandra/cas_case2/qsub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#$ -N cas_{pressure}
+#$ -N cas_2_{pressure}
 #$ -cwd
-#$ -l h_rt=06:00:00
+#$ -l h_rt=12:00:00
 #$ -l h_vmem=3G
 
 . /etc/profile.d/modules.sh
@@ -10,7 +10,7 @@ ulimit -s unlimited
 
 cat /proc/cpuinfo > cpuinfo.{pressure}
 
-echo "Timing Cassandra case 1 at pressure {pressure}" > timing.$JOB_ID
+echo "Timing Cassandra case 2 at pressure {pressure}" > timing.$JOB_ID
 date >> timing.$JOB_ID
 ./cassandra.exe CO2_IRMOF.inp
 date >> timing.$JOB_ID
