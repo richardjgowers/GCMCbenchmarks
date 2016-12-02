eac1a476004f71a8508683f99d8e4fae98e4589a
Richard Gowers <richardjgowers@gmail.com>
2016-12-02 14:25:46 +0000

    Added skeleton for results parsing

---
+++ gcmcbenchmarks/parsers/__init__.py
@@ -0,0 +1,9 @@
+# parsers for each program
+
+from . import cassandra_parser
+from . import dlmonte_parser
+from . import music_parser
+from . import raspa_parser
+from . import towhee_parser
+
+from . import parser
---
+++ gcmcbenchmarks/parsers/parser.py
@@ -0,0 +1,9 @@
+
+from . import (
+    cassandra_parser as cas,
+    dlmonte_parser as dlm,
+    music_parser as mus,
+    raspa_parser as rsp,
+    towhee_parser as twh,
+)
+
