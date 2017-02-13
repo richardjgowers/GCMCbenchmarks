5de49482f852662b2d7ccbecf53b1743f24f89d6
Richard Gowers <richardjgowers@gmail.com>
2017-02-13 16:39:00 +0000

    Bumped version to 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), 'r') as f:
 
 setup(
     name = 'gcmcbenchmarks',
-    version = '0.0.1',
+    version = '0.0.2',
     long_description=long_description,
 
     packages=find_packages(),
