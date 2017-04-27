9b3db48c0127fa45b17eddf09589ed6ee6b26f3b
Richard Gowers <richardjgowers@gmail.com>
2017-04-27 11:59:11 +0100

    eq value not required for find_tau

--- gcmcbenchmarks/analysis/tau.py
+++ gcmcbenchmarks/analysis/tau.py
@@ -14,14 +14,17 @@ def grab_until(sig, thresh):
 
     return sig[:cut].iloc[:-1]  # return signal up to cut, excluding cut
 
+
 def grab_after(sig, thresh):
     """Works on falling signals"""
     cut = sig[sig < thresh].index[0]
     
     return sig[cut:]
 
+
 def exp_fit(x, tau):
     return np.exp(-x/tau)
+
 
 def do_exp_fit(sig, thresh=0.1):
     """Fit an exponential up to thresh
@@ -46,23 +49,29 @@ def do_exp_fit(sig, thresh=0.1):
     x, y = sig.index, sig.values
     return curve_fit(exp_fit, x, y, p0=10000)[0][0]
 
-def find_tau(sig, eq, tmax=5000000, thresh=0.1):
+
+def find_tau(sig, tmax=5000000, thresh=0.1):
     """
     Parameters
     ----------
     sig : pd.Series
-      the entire signal, including equilibrium phase
-    eq : int
-      the number of steps before equilibration
+      the signal to calculate tau from.  It is important that the
+      equilibrium period has been removed
     tmax : int, optional
       the total number of steps to calculate the autocorrelation for
     thresh : float, optional
       Value until which the ACF is fitted to [Default: 0.1]
+
+    Returns
+    -------
+    tau value of decorrelation
     """
     # find how many rows of sig we will use
-    nlags = sig[:tmax].shape[0]
-    acf = stattools.acf(sig[eq:], fft=True, nlags=nlags)
-    # +1 as acf at zero is returned
-    acf = pd.Series(acf, sig.index[:nlags + 1])
+    # the signal won't start at t=0, therefore find the offset and adjust
+    t0 = sig.index[0]
+    nlags = sig[:t0 + tmax].shape[0]
+    acf = stattools.acf(sig, fft=True, nlags=nlags)
+    # (nlags + 1) as acf at zero is returned
+    acf = pd.Series(acf, sig.index[:nlags + 1] - t0)
 
     return do_exp_fit(acf, thresh=thresh)
